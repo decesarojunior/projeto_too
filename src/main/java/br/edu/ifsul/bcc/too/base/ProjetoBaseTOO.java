7fda61da4a49d1780fdcdc27f3968c05f220d7ef
decesarojunior <decesarojunior@yahoo.com.br>
2022-03-31 10:30:16 -0300

    implementação final para a disciplina de LPOO 2021/2.

---
+++ src/main/java/br/edu/ifsul/bcc/too/base/ProjetoBaseTOO.java
@@ -0,0 +1,17 @@
+
+package br.edu.ifsul.bcc.too.base;
+
+/**
+ *
+ * @author telmo Junior
+ * @data 31/03/22021
+ */
+public class ProjetoBaseTOO {
+        
+    //Fonte: https://www.devmedia.com.br/entendendo-e-conhecendo-as-versoes-do-java/25210
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+    }
+}
