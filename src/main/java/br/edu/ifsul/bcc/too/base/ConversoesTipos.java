2867efd6419924601411547ba04134ab9c17b038
decesarojunior <decesarojunior@yahoo.com.br>
2022-04-01 16:49:42 -0300

    Tópico 1: variáveis, convenções, tipos de dados, strings, conversões, arrays e tratamento de exceçoes.

---
+++ src/main/java/br/edu/ifsul/bcc/too/base/ConversoesTipos.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsul.bcc.too.base;
+
+/**
+ *
+ * @author telmo
+ */
+public class ConversoesTipos {
+    
+    
+    public static void main(String[] args) {
+        
+        /* Conversões de tipos */
+        
+        String strValor1 = "123";
+        
+        String strValor2 = "123a";
+        
+        String strValor3 = "10.5";
+        
+        Integer iValor = Integer.parseInt(strValor1);
+        
+        iValor = Integer.valueOf(strValor1);                
+        
+        Float fValor = Float.parseFloat(strValor3);
+        
+        String strTeste = fValor.toString();
+        
+    }
+    
+}
+
+
+    /*
+        Fontes: 
+            https://www.devmedia.com.br/conversoes-em-java/2695            
+    */
--- src/main/java/br/edu/ifsul/bcc/too/base/ProjetoBaseTOO.java
+++ src/main/java/br/edu/ifsul/bcc/too/base/ProjetoBaseTOO.java
@@ -7,11 +7,30 @@ package br.edu.ifsul.bcc.too.base;
  * @data 31/03/22021
  */
 public class ProjetoBaseTOO {
-        
-    //Fonte: https://www.devmedia.com.br/entendendo-e-conhecendo-as-versoes-do-java/25210
-
+    
     public static void main(String[] args) {
+    
+    /*Variáveis: 
+        As variáveis são posições na memória do computador que podem armazenar dados. 
+        As variáveis são formadas por quatro elementos: nome, tipo, tamanho e valor. 
+        Dependendo da programação, o básico de uma declaração de variável pode ter somente um tipo, um nome e um valor.    
+    */
+    
+    int a;
+    int b = 0;
+    float c = 1;
+    String d = "";
+    int[] vetor;
+    vetor = new int[10];
+    int[] array = {1,2,3};
+    char e;
+    e = 'A';
+    boolean t;
+    
+    System.out.println(c);
         
-        System.out.println("Hello World!");
+        
     }
 }
+
+   
