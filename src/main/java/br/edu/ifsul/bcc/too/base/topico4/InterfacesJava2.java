47b5ac77b3da5de677a7455933175794538c6eb6
decesarojunior <decesarojunior@yahoo.com.br>
2022-07-01 09:41:27 -0300

    diagrama de classe: exemplos e exercicios.

---
+++ src/main/java/br/edu/ifsul/bcc/too/base/topico4/InterfacesJava2.java
@@ -0,0 +1,21 @@
+
+package br.edu.ifsul.bcc.too.base.topico4;
+
+/**
+ *
+ * @author telmo
+ */
+public class InterfacesJava2 {
+    
+    InterfacesJava2(){
+        
+    }
+    
+    
+    public static void main(String args[]){
+        
+        new InterfacesJava();
+        
+    }
+    
+}
